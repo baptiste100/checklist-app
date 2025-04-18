437161aeb3325d95cd2f6cbd1c5b7d2a0597069e
baptistekieffer <baptiste67.k@gmail.com>
2025-04-18 22:43:56 +0200

    feat(db): utilisation d'un Singleton

--- app/src/main/java/perso/checklistapp/db/AppDatabase.kt
+++ app/src/main/java/perso/checklistapp/db/AppDatabase.kt
@@ -1,6 +1,8 @@
 package perso.checklistapp.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import perso.checklistapp.dao.TaskDao
 import perso.checklistapp.model.Task
@@ -8,4 +10,21 @@ import perso.checklistapp.model.Task
 @Database(entities = [Task::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "checklist-app-db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
