c76b095ffc2cae2957e644a72945389cf4c63bb8
baptistekieffer <baptiste67.k@gmail.com>
2025-04-19 10:23:45 +0200

    refactor(dao): Utilisation de Flow<List<Task>> pour getAll()

--- app/src/main/java/perso/checklistapp/dao/TaskDao.kt
+++ app/src/main/java/perso/checklistapp/dao/TaskDao.kt
@@ -1,11 +1,13 @@
 package perso.checklistapp.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import perso.checklistapp.model.Task
 
 @Dao
 interface TaskDao {
     @Query("SELECT * FROM task")
-    fun getAll(): List<Task>
+    fun getAll(): Flow<List<Task>>
 }
