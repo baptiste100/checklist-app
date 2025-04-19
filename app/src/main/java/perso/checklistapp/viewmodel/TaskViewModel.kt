97ddd948c4259483e010b5a4e6b98fd371476bc0
baptistekieffer <baptiste67.k@gmail.com>
2025-04-19 10:45:49 +0200

    feat(viewmodel): création de TaskViewModel

---
+++ app/src/main/java/perso/checklistapp/viewmodel/TaskViewModel.kt
@@ -0,0 +1,21 @@
+package perso.checklistapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import perso.checklistapp.db.AppDatabase
+import perso.checklistapp.model.Task
+
+class TaskViewModel(application: Application) : ViewModel() {
+    private val _taskDao = AppDatabase.getInstance(application).taskDao()
+    val _allTasks: LiveData<List<Task>> = _taskDao.getAll().asLiveData()
+
+    fun insert(task: Task) = viewModelScope.launch {
+        _taskDao.insert(task)
+    }
+}
