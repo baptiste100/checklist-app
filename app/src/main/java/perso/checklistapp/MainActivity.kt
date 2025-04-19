0b1c2bd0128cc13424f20a5a7a1145d3b73654a1
baptistekieffer <baptiste67.k@gmail.com>
2025-04-19 16:42:05 +0200

    style: suppression du thème sombre

--- app/src/main/java/perso/checklistapp/MainActivity.kt
+++ app/src/main/java/perso/checklistapp/MainActivity.kt
@@ -3,6 +3,7 @@ package perso.checklistapp
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
